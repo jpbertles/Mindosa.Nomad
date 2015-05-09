f3e22fe2c5d2a1a3b87fdbc7185b63e68584f7f0
Joseph P. Bertles <jbertles@innovativesol.com>
2015-05-08 20:19:04 -0400

    somehow managed to forget these two updates in the refactor commit

--- src/Mindosa.Nomad/Mindosa.Nomad.Core/Repositories/Abstract/IMigrationRepository.cs
+++ src/Mindosa.Nomad/Mindosa.Nomad.Core/Repositories/Abstract/IMigrationRepository.cs
@@ -9,8 +9,8 @@ namespace Mindosa.Nomad.Core.Repositories.Abstract
 {
     public interface IMigrationRepository
     {
-        List<MigrationMetaData> GetInfo(string connectionString);
-        void ApplyMigration(MigrationFile migrationFile, string connectionString);
-        void SetBaseline(string connectionString);
+        List<Entities.MigrationMetaData> GetInfo();
+        void SetBaseline();
+        void ApplyMigration(Entities.MigrationFile migrationFile);
     }
 }
